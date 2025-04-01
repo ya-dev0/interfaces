e9ef330c21cc9d38e638019dcec8f43bfd11a8c7
Artem Stebletskiy <demoyuam1@proton.me>
2025-04-01 15:31:04 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+// pages/index.tsx
 import React, { useState } from 'react';
 import BurgerMenu from '../components/BurgerMenu';
 import Practice1 from '../components/Practice1';
 import Practice2 from '../components/Practice2';
 import Practice3 from '../components/Practice3';
-import Practice4 from '../components/Practice4';
+import Practice4 from '../components/Practice4'; // Переписанная практика 4
 
 const IndexPage: React.FC = () => {
   const [currentTab, setCurrentTab] = useState<string>('practice1');
@@ -15,7 +16,6 @@ const IndexPage: React.FC = () => {
   return (
     <div className="min-h-screen">
       <BurgerMenu onTabChange={handleTabChange} />
-
       {currentTab === 'practice1' && <Practice1 />}
       {currentTab === 'practice2' && <Practice2 />}
       {currentTab === 'practice3' && <Practice3 />}
