dce25bb3d8a43887af582b33146ac170cd51c4d4
Artem Stebletskiy <demoyuam1@proton.me>
2025-04-03 15:33:23 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
-import '../styles/globals.css';
+// pages/_app.tsx
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
